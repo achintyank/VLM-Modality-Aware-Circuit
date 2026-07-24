0cc61d17ace8227952d6dd03ab57abd3ab013254
someperson6 <126364420+someperson6@users.noreply.github.com>
2026-07-23 21:24:36 -0700

    reorganize delete some files

--- candidate_vector_experiment-achin-justin/justin/config.py
+++
@@ -1,3 +0,0 @@
-CANDIDATE_LAYERS = [3, 14, 26]   # hidden_states indices to test as candidate modality vectors
-N_PAIRS = 50                      # number of PixelProse pairs used to build the vectors
-PROMPT_TEXT = "What is the image?"
